216bbce8bf8c75072289119cfb07bc65aff16eb0
Hari Rajagopal <97304924+harir2022@users.noreply.github.com>
2023-02-28 00:09:19 +0530

    Update files.ts

--- backend/src/handlers/files.ts
+++ backend/src/handlers/files.ts
@@ -23,52 +23,59 @@ export default function mountServiceEndpoints(router: Router) {
   // handle the user auth accordingly
   router.post('/upload',async(req: Request, res: Response) => {
 
-        const auth = req.body.uid
-        if(!auth && auth==undefined) 
-          {
-            // //("login first")
-            return res.json({
-            success:false,
-            msg:"login first"              
-          })        
-        }
-        const store= req.app.locals.store;
-        const userCollection= req.app.locals.userCollection;
+    try {
+      const auth = req.body.uid
+      if(!auth && auth==undefined) 
+        {
+          // //("login first")
+          return res.json({
+          success:false,
+          msg:"login first"              
+        })        
+      }
+      const store= req.app.locals.store;
+      const userCollection= req.app.locals.userCollection;
+  
+      // const url="asdf";
+       const cloud=  await cloudinaryUploader(auth,req.body.image);
+       const imageObject= {
+           url: cloud.url,
+           name:req.body.imageName
+      }
+        
+        // //(req.body.uid)
+   
+        let currentUser = await userCollection.findOne({ uid: auth});
+        // //(currentUser)
     
-        // const url="asdf";
-         const cloud=  await cloudinaryUploader(auth,req.body.image);
-         const imageObject= {
-             url: cloud.url,
-             name:req.body.imageName
-        }
-          
-          // //(req.body.uid)
-     
-          let currentUser = await userCollection.findOne({ uid: auth});
-          // //(currentUser)
-      
-          try {
-            const insertedRow = await store.insertOne({
-              uid:auth,
-              image: imageObject,
-              date: Date.now()
-          })
-          //("inserted a row ")
-          //(insertedRow)
-       
-        res.status(200).json({
-          success:true,
-          msg:"successfully uploaded"
-          
+        try {
+          const insertedRow = await store.insertOne({
+            uid:auth,
+            image: imageObject,
+            date: Date.now()
         })
-            
-          } catch (error) {
-            //(error)
-            return res.status(400).json({
-              success:false
-            })
-          }
+        //("inserted a row ")
+        //(insertedRow)
+     
+      res.status(200).json({
+        success:true,
+        msg:"successfully uploaded"
+        
+      })
           
+        } catch (error) {
+          //(error)
+          return res.status(400).json({
+            success:false
+          })
+        }
+        
+    } catch (error) {
+      res.json({
+        success:false,
+      })
+    }
+       
           
    });
 
@@ -76,28 +83,35 @@ export default function mountServiceEndpoints(router: Router) {
    //get images;
 
    router.get('/gallery/:uid',async(req:Request,res:Response)=>{
-    const userUid= req.params.uid
-    // //(req.params.uid)
-    if(!userUid){
-      return res.status(404).json({
-        success:false,
-        msg:"Login in first"
-      })
-    }
-    //(req.body)
-    const store= req.app.locals.store;
-    let images:object[]=[];
-    const results = await store.find({uid:userUid}).toArray().then((d:any)=>{
-      for(let ch of d){
-        images.push(ch.image);
-      }
-    });
-    
-    //(images)
-    res.json({
-      images
-    })
-
+        try {
+          const userUid= req.params.uid
+          // //(req.params.uid)
+          if(!userUid){
+            return res.status(404).json({
+              success:false,
+              msg:"Login in first"
+            })
+          }
+          //(req.body)
+          const store= req.app.locals.store;
+          let images:object[]=[];
+          const results = await store.find({uid:userUid}).toArray().then((d:any)=>{
+            for(let ch of d){
+              images.push(ch.image);
+            }
+          });
+          
+          //(images)
+          res.json({
+            images
+          })
+      
+        } catch (error) {
+          return res.json({
+            success:false,
+            error
+          })
+        }
    })
 
 
@@ -106,7 +120,8 @@ export default function mountServiceEndpoints(router: Router) {
 
   // handle the user auth accordingly
   router.get('/list', async (req: Request, res: Response) => {
-    
+      try {
+           
     const userCollection= req.app.locals.userCollection;
     
     let users: any[]=[];
@@ -125,7 +140,14 @@ export default function mountServiceEndpoints(router: Router) {
       success:true,
       users
     })
-  });
+      } catch (error) {
+        return res.status(400).json({
+          success:false,
+          error
+        })
+      }
+  }
+  );
 
 
 
@@ -138,7 +160,8 @@ export default function mountServiceEndpoints(router: Router) {
     // url:string,
     // access:Boolean
   router.post('/share',async(req:Request , res:Response)=>{
-        const users= req.body.users;
+        try {
+          const users= req.body.users;
         const owner= req.body.authRes.user.uid;
         const sharedCollection = req.app.locals.sharedCollection;
         const url = req.body.url;
@@ -156,24 +179,36 @@ export default function mountServiceEndpoints(router: Router) {
          res.status(200).json({
           success:true
          })
+        } catch (error) {
+          res.status(400).json({
+            success:false
+           })
+        }
   })
 
 
   //get images for viewing
   router.get('/inbox' ,async (req:Request,res:Response)=>{
+     try {
+      
       const auth = req.body.authRes.user.uid;
-        const sharedCollection = req.app.locals.sharedCollection;
-      const images:any[]=[];
-        const result = await sharedCollection.find({toId:auth}) .toArray().then((d:any)=>{
-              for(var ch of d){
-                images.push(ch.url);
-              }
-        })
-        // //(images)
-        res.status(200).json({
-          success:true,
-          images
-        })
+      const sharedCollection = req.app.locals.sharedCollection;
+    const images:any[]=[];
+      const result = await sharedCollection.find({toId:auth}) .toArray().then((d:any)=>{
+            for(var ch of d){
+              images.push(ch.url);
+            }
+      })
+      // //(images)
+      res.status(200).json({
+        success:true,
+        images
+      })
+     } catch (error) {
+      return res.json({
+        success:false
+      })
+     }
   })
 
 
